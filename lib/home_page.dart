4abfb799daefaea5979e72546382e80150d5a3f6
Kshitij Passi <kshitijnishu@gmail.com>
2019-10-15 15:18:24 +0530

    Complete

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -11,43 +11,51 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title:  new Text("Tic Tac Toe"),),
+      appBar: new AppBar(
+        title: new Text("Tic Tac Toe"),
+      ),
       body: Container(
         decoration: new BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [Color.fromRGBO(52, 152, 219, 100),Color.fromRGBO(142, 68, , 100)]
-          ),
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Color.fromRGBO(52, 152, 219, 100),
+                Color.fromRGBO(142, 68, 173, 100)
+              ]),
         ),
         width: 500,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 295.0),
-            child: Column(
-              children: <Widget>[
-                FlatButton(
-                  child: FlatButton(
-                    color: Colors.black,
-                    colorBrightness: Brightness.dark,
-                    child: Text("SinglePlayer"),
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>SinglePlayer()));
-                    },
-                  ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 295.0),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                child: FlatButton(
+                  color: Colors.black,
+                  colorBrightness: Brightness.dark,
+                  child: Text("SinglePlayer"),
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SinglePlayer()));
+                  },
                 ),
-                FlatButton(
-                  child: FlatButton(
-                    color: Colors.red,
-                    colorBrightness: Brightness.dark,
-                    child: Text("Vs Mode"),
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=>AutoPlay()));
-                    },
-                  ),
+              ),
+              FlatButton(
+                child: FlatButton(
+                  color: Colors.red,
+                  colorBrightness: Brightness.dark,
+                  child: Text("Vs Mode"),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => AutoPlay()));
+                  },
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
+        ),
       ),
     );
   }
